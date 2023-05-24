79636fbbad0059fa8cdbd69f162e4163e566f5f9
fadwdwa <fawda1119@gmail.com>
2023-05-24 16:02:03 +0100

    updated the code

--- main.h
+++ main.h
@@ -4,21 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 extern char **environ;
 
-/* EXECUTION */
-char *find_executable_path(char *command);
-void execute_command(char **tokens);
-
-/* COMMANDS */
-int handle_exit_command(char *command);
-int handle_env_command(char *command);
-
-/* TOKENIZER */
-char **tokenize_command(char *command);
-void free_tokens(char **tokens);
 #endif
--- shell.c
+++ shell.c
@@ -1,51 +1,208 @@
 #include "main.h"
 
+char *find_path(char *command);
+
 /**
- * main - Entry point of the shell program
- * @ac: The number of command-line arguments
- * @av: Array of command-line arguments
- * Return: Always 0
+ * read_line - Read a line of input from stdin.
+ * Return: the read line.
  */
-int main(int ac, char **av)
+char *read_line()
 {
-	char *command = NULL, **tokens = NULL;
-	size_t command_len = 0;
+	char *line = NULL;
+	size_t line_len = 0;
 	ssize_t nget;
+
+	printf("$ ");
+	nget = getline(&line, &line_len, stdin);
+
+	if (nget == -1)
+	{
+		perror("getline");
+		exit(EXIT_FAILURE);
+	}
+
+	if (line[nget - 1] == '\n')
+		line[nget - 1] = '\0';
+
+	return (line);
+}
+
+/**
+ * tokenize_line - Tokenize the input line.
+ * @line: ptr to char
+ * Return: an array of tokens.
+ */
+char **tokenize_line(char *line)
+{
+	char *token, **tokens = NULL;
+	int num_tokens = 0;
+
+	token = strtok(line, " \t\n");
+	while (token != NULL)
+	{
+		tokens = realloc(tokens, (num_tokens + 1) * sizeof(char *));
+		if (tokens == NULL)
+		{
+			perror("realloc");
+			exit(EXIT_FAILURE);
+		}
+
+		tokens[num_tokens] = strdup(token);
+		num_tokens++;
+		token = strtok(NULL, " \t\n");
+	}
+
+	tokens = realloc(tokens, (num_tokens + 1) * sizeof(char *));
+	tokens[num_tokens] = NULL;
+
+	return (tokens);
+}
+
+/**
+ * free_tokens - Free the allocated memory for the tokens.
+ * @tokens: double ptr to tokens
+ * Return: void
+ */
+void free_tokens(char **tokens)
+{
+	char **ptr = tokens;
+
+	while (*ptr != NULL)
+	{
+		free(*ptr);
+		ptr++;
+	}
+
+	free(tokens);
+}
+
+/**
+ * execute_command - Execute the command with the specified tokens.
+ * @tokens: double pointer to tokens
+ * Return: void
+ */
+void execute_command(char **tokens)
+{
+	char *cmd_path;
 	pid_t pid;
 
-	(void)ac, (void)av;
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+
+		cmd_path = find_path(tokens[0]);
+
+		tokens[0] = cmd_path;
+
+		if (execve(tokens[0], tokens, NULL) == -1)
+		{
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		waitpid(pid, NULL, 0);
+	}
+}
+/**
+ * find_path - Find the path of the command
+ * in the system's PATH variable.
+ * @command: the command
+ * Return: the path if found, otherwise returns NULL.
+ */
+char *find_path(char *command)
+{
+	char *path, *dir_path, *new_path;
+	int dir_len, cmd_len;
+	struct stat st;
+
+	path = getenv("PATH");
+
+	if (stat(command, &st) == 0)
+		return (command);
+
+	if (path)
+	{
+		cmd_len = strlen(command);
+		dir_path = strtok(path, ":");
+
+		while (dir_path != NULL)
+		{
+			dir_len = strlen(dir_path);
+			new_path = calloc(dir_len + cmd_len + 2, sizeof(char));
+
+			if (!new_path)
+			{
+				perror("calloc");
+				exit(EXIT_FAILURE);
+			}
+
+			strcpy(new_path, dir_path);
+			strcat(new_path, "/");
+			strcat(new_path, command);
+
+			if (stat(new_path, &st) == 0)
+				return (new_path);
+
+			free(new_path);
+			dir_path = strtok(NULL, ":");
+		}
+	}
+
+	return (NULL);
+}
+
+/**
+ * main - the main fct
+ * Return: 0
+ */
+int main(void)
+{
+	char *command;
+	char **tokens;
+
 	while (1)
 	{
-		printf("$ ");
-		nget = getline(&command, &command_len, stdin);
-		if (nget == -1)
-		{
-			perror("getline");
-			exit(EXIT_FAILURE);
-		}
-		if (command[nget - 1] == '\n')
-			command[nget - 1] = '\0';
-		if (command[0] == '\0')
-			continue;
-		if (handle_exit_command(command) || handle_env_command(command))
-			continue;
-		pid = fork();
-		if (pid == -1)
-		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == 0)
-		{
+		command = read_line();
 
-			tokens = tokenize_command(command);
-			execute_command(tokens);
-			free_tokens(tokens);
+		if (command[0] == '\0')
+		{
+			free(command);
+			continue;
+		}
+
+		if (strcmp("exit", command) == 0)
+		{
+			free(command);
 			exit(EXIT_SUCCESS);
 		}
-		else
-			waitpid(pid, NULL, 0);
+
+		if (strcmp("env", command) == 0)
+		{
+			char **env = environ;
+
+			while (*env != NULL)
+			{
+				printf("%s\n", *env);
+				env++;
+			}
+
+			free(command);
+			continue;
+		}
+
+		tokens = tokenize_line(command);
+		free(command);
+
+		execute_command(tokens);
+		free_tokens(tokens);
 	}
-	free(command);
+
 	return (0);
 }
