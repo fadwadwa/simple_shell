d15774ae119e1e58d4ce49940ac3800d247ab8c0
fadwdwa <fawda1119@gmail.com>
2023-05-26 17:27:00 +0100

    updating exit with invalid arguments

--- shell.c
+++ shell.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdint.h>
 
 char *find_path(char *command);
 /**
@@ -261,7 +260,7 @@ int main(void)
 		tokens = tokenize_line(command);
 		if (strcmp("exit", command) == 0)
 		{
-			if (tokens[1] != NULL && ((intptr_t)tokens[1] >= 0 && (intptr_t)tokens[1] <= 255))
+			if (tokens[1] != NULL && (atoi(tokens[1]) >= 0 && atoi(tokens[1]) <= 255))
 				last_status = atoi(tokens[1]);
 			free(command);
 			free_tokens(tokens);
